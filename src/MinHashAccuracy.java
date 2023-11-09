55927d1d2733301ec96231868f9fa6decb937ae1
shiv-neel <shiv.neel1622@gmail.com>
2023-11-09 03:07:04 -0600

    done + report

--- src/MinHashAccuracy.java
+++ src/MinHashAccuracy.java
@@ -25,22 +25,22 @@ public class MinHashAccuracy {
     public static void main(String[] args) {
         MinHashAccuracy minHashAccuracy = new MinHashAccuracy();
         int[] accuraciesFor400Permutations = new int[3];
-        accuraciesFor400Permutations[0] = minHashAccuracy.accuracy("data/test", 400, 0.04);
-        accuraciesFor400Permutations[1] = minHashAccuracy.accuracy("data/test", 400, 0.07);
-        accuraciesFor400Permutations[2] = minHashAccuracy.accuracy("data/test", 400, 0.09);
+        accuraciesFor400Permutations[0] = minHashAccuracy.accuracy("data/space", 400, 0.04);
+        accuraciesFor400Permutations[1] = minHashAccuracy.accuracy("data/space", 400, 0.07);
+        accuraciesFor400Permutations[2] = minHashAccuracy.accuracy("data/space", 400, 0.09);
 
         int[] accuraciesFor600Permutations = new int[3];
-        accuraciesFor600Permutations[0] = minHashAccuracy.accuracy("data/test", 600, 0.04);
-        accuraciesFor600Permutations[1] = minHashAccuracy.accuracy("data/test", 600, 0.07);
-        accuraciesFor600Permutations[2] = minHashAccuracy.accuracy("data/test", 600, 0.09);
+        accuraciesFor600Permutations[0] = minHashAccuracy.accuracy("data/space", 600, 0.04);
+        accuraciesFor600Permutations[1] = minHashAccuracy.accuracy("data/space", 600, 0.07);
+        accuraciesFor600Permutations[2] = minHashAccuracy.accuracy("data/space", 600, 0.09);
 
         int[] accuraciesFor800Permutations = new int[3];
-        accuraciesFor800Permutations[0] = minHashAccuracy.accuracy("data/test", 800, 0.04);
-        accuraciesFor800Permutations[1] = minHashAccuracy.accuracy("data/test", 800, 0.07);
-        accuraciesFor800Permutations[2] = minHashAccuracy.accuracy("data/test", 800, 0.09);
+        accuraciesFor800Permutations[0] = minHashAccuracy.accuracy("data/space", 800, 0.04);
+        accuraciesFor800Permutations[1] = minHashAccuracy.accuracy("data/space", 800, 0.07);
+        accuraciesFor800Permutations[2] = minHashAccuracy.accuracy("data/space", 800, 0.09);
 
         // Print table rows
-        System.out.format("%s%10s%10s%10s%n", " ", "epsilon = 0.04", "epsilon = 0.07", "epsilon = 0.09");
+        System.out.format("%s%20s%10s%10s%n", "Epsilon", "0.04", "0.07", "0.09");
         System.out.format("%s%10d%10d%10d%n", "400 permutations:", accuraciesFor400Permutations[0], accuraciesFor400Permutations[1], accuraciesFor400Permutations[2]);
         System.out.format("%s%10d%10d%10d%n", "600 permutations:", accuraciesFor600Permutations[0], accuraciesFor600Permutations[1], accuraciesFor600Permutations[2]);
         System.out.format("%s%10d%10d%10d%n", "800 permutations:", accuraciesFor800Permutations[0], accuraciesFor800Permutations[1], accuraciesFor800Permutations[2]);
