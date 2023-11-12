42eaecd0341a84aaf2cc9a8e3f7363efb82673f5
Ritvik Ambekar <ritvikemail@gmail.com>
2023-11-12 16:01:37 -0600

    lsh created

--- src/LSH.java
+++ src/LSH.java
@@ -1,61 +1,62 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class LSH {
-   // private int[][] minHashMatrix; Need to replace with our MinHash matrix
+    //minHash matrix 
+    private int[][] minHashMatrix; 
+    // string array of document names that are within the matrix
     private String[] docNames;
-    private int bands;
-    private int rowsPerBand;
-    private int k;
+    // number of lsh bands
     private int b;
-    private List<Map<Integer, List<String>>> hashTables;
+    // number of rows per band
+    private int rowsPerBand;
+    // number of permutations 
+    private int k;
+    // lsh result
+    private List<HashMap<Integer, List<String>>> hashTables = new ArrayList<HashMap<Integer, List<String>>>();
 
-    public LSH(int[][] minHashMatrix, String[] docNames, int bands) {
-      //  this.minHashMatrix = minHashMatrix;
+    public LSH(int[][] minHashMatrix, String[] docNames, int b) {
+        this.minHashMatrix = minHashMatrix;
         this.docNames = docNames;
-        this.bands = bands;
-        this.k = minHashMatrix.length;
-        this.b = minHashMatrix[0].length;
-        this.rowsPerBand = k / bands;
-        this.hashTables = new ArrayList<>();
+        this.b = b;
+        this.k= minHashMatrix[0].length;
+        this.rowsPerBand = k / b;
 
         initializeLSH();
     }
 
     private void initializeLSH() {
-        for (int i = 0; i < bands; i++) {
-            Map<Integer, List<String>> hashTable = new HashMap<>();
-            for (int j = 0; j < b; j++) {
-                int bandHash = computeBandHash(i, j);
-                if (!hashTable.containsKey(bandHash)) {
-                    hashTable.put(bandHash, new ArrayList<>());
+        for (int g = 0; g < b; g++) {
+            hashTables.add(new HashMap<Integer, List<String>>());
+            for (int i = 0; i < docNames.length; i++) {
+                for (int j = 0; j < b; j++) {
+                    HashMap<Integer, List<String>> tempTable = hashTables.get(j);
+                    int hashValue = Arrays.hashCode(Arrays.copyOfRange(minHashMatrix[i], rowsPerBand * j, rowsPerBand * j + rowsPerBand));
+
+                 if (!tempTable.containsKey(hashValue)) {
+                     tempTable.put(hashValue, new ArrayList<String>());
+                    }
+
+                tempTable.get(hashValue).add(docNames[i]);
                 }
-                hashTable.get(bandHash).add(docNames[j]);
-            }
-            hashTables.add(hashTable);
-        }
-    }
-    public List<String> retrieveSim(String docName) {
-        List<String> similarDocs = new ArrayList<>();
-
-        for (int i = 0; i < bands; i++) {
-            int bandHash = computeBandHash(i, findDocIndex(docName));
-            if (hashTables.get(i).containsKey(bandHash)) {
-                similarDocs.addAll(hashTables.get(i).get(bandHash));
             }
         }
-
-        return similarDocs;
     }
-
-    private int findDocIndex(String docName) {
-        for (int i = 0; i < docNames.length; i++) {
-            if (docNames[i].equals(docName)) {
-                return i;
-            }
+    //finds duplicates of a given document
+    public ArrayList<String> retrieveSim (String docName) {
+        int docIndex = Arrays.asList(docNames).indexOf(docName);
+        Set<String> temp = new HashSet<String>();
+        // do lsh in order to get similar documents
+        for (int i = 0; i < b; i++) {
+        HashMap<Integer, List<String>> tempMap = hashTables.get(i);
+        int hashValue =Arrays.hashCode(Arrays.copyOfRange(minHashMatrix[docIndex], rowsPerBand * i, rowsPerBand * i + rowsPerBand));
+        List<String> similarDocs = tempMap.get(hashValue);
+        for (String doc : similarDocs) {
+            temp.add(doc);
         }
-        return -1; // Doc not found
-    }
+        }
+        temp.remove(docName);
+        ArrayList<String> nearDups = new ArrayList<String>();
+        nearDups.addAll(temp);
+        return nearDups;
+  }
 }
